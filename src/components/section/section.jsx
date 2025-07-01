83e84d76ba5da1c4f2fa99092a5935761ba96507
andres <asve1315@gmail.com>
2025-07-01 17:45:03 -0500

    pruebas

--- src/components/section/section.jsx
+++ src/components/section/section.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import UserCard from '../UserCard/UserCard';
 import appleimg from '../../assets/apple.png';
 import lemonimg from '../../assets/lemon.png';
@@ -30,14 +30,50 @@ const frutas = [
 ];
 
 export default function Section() {
+  const [count, setCount] = useState(0);
+  const [users, setUsers] = useState([]);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/users/filter?key=hair.color&value=Brown')
+      .then(res => res.json())
+      .then(data => {
+        setUsers(data.users);
+      });
+  }, [count]);
+
   return (
-    <section className="fruta-section">
-      <h1 className="fruta-titulo">Total de frutas: {frutas.length}</h1>
-      <div className="card-container">
-        {frutas.map((fruta) => (
-          <UserCard key={fruta.id} user={fruta} />
-        ))}
-      </div>
-    </section>
+    <div>
+      <h2>Contador: {count}</h2>
+      <button onClick={handleClick}>Cargar usuarios</button>
+
+      {/* Sección de frutas */}
+      <section className="fruta-section">
+        <h1 className="fruta-titulo">Total de frutas: {frutas.length}</h1>
+        <div className="card-container">
+          {frutas.map((fruta) => (
+            <UserCard key={fruta.id} user={fruta} />
+          ))}
+        </div>
+      </section>
+
+      {/* Sección de usuarios */}
+      <section className="usuarios-section">
+        <h1 className="fruta-titulo">Usuarios : {users.length}</h1>
+        <div className="card-container">
+          {users.map((user) => (
+            <div key={user.id} className="user-card">
+              <img src={user.image} alt={user.firstName} className="user-img" />
+              <h3>{user.firstName} {user.lastName}</h3>
+              <p>Email: {user.email}</p>
+              <p>Edad: {user.age}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
   );
 }
